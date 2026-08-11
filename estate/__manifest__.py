96d9ba7d56bf88d6bbb6c0abc057e929f06a92a7
carlo191 <carloelvisi01@outlook.it>
2026-08-11 23:30:06 +0200

    Creato modulo estate con modello estate.property

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'summary': 'Real estate management module',
+    'depends': ['base'],
+    'author': 'Carlo',
+    "license": "OPL-1",
+    "data": []
+  
+}
