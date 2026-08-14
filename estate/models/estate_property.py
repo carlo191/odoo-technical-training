9bf238398c26ab1f4664adfe0f6301341b0fa454
carlo191 <carloelvisi01@outlook.it>
2026-08-14 23:01:21 +0200

    Aggiunta vista lista per estate.property con colonne personalizzate

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,24 +5,27 @@ from dateutil.relativedelta import relativedelta
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
+
     def default_date(self):
         return fields.Date.today() + relativedelta(months=3)
 
     name = fields.Char(required=True)
     price = fields.Float()
-    selling_price = fields.Float(readonly=True,copy=False)
+    selling_price = fields.Float(readonly=True, copy=False)
     date_availability = fields.Date(copy=False, default=default_date)
     bedrooms = fields.Integer(default=2)
     active = fields.Boolean(default=True)
+    postcode = fields.Char()
+    living_area = fields.Integer()
     state = fields.Selection(
-    selection=[
-        ('new', 'New'),
-        ('offer_received', 'Offer Received'),
-        ('offer_accepted', 'Offer Accepted'),
-        ('sold', 'Sold'),
-        ('canceled', 'Canceled'),
-    ],
-    required=True,
-    copy=False,
-    default='new',
-)
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Canceled'),
+        ],
+        required=True,
+        copy=False,
+        default='new',
+    )
