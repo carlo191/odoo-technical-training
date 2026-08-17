b64944ab2c0326cf47e47319a56282498f2680f2
carlo191 <carloelvisi01@outlook.it>
2026-08-17 11:44:07 +0200

    Aggiunte relazioni one2many e many2many con modelli offer e tag per estate.property

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,12 +5,14 @@
     'depends': ['base'],
     'author': 'Carlo',
     'license': 'OPL-1',
-    'data': [
-        'security/security.xml',
-        'security/ir.model.access.csv',
-        'views/estate_property_views.xml',
-        'views/estate_menus.xml',
-    ],
+'data': [
+    'security/security.xml',
+    'security/ir.model.access.csv',
+    'views/estate_property_views.xml',
+    'views/estate_property_offer_views.xml',
+    'views/estate_property_tag_views.xml',
+    'views/estate_menus.xml',
+],
     'demo': [
         'demo/demo.xml',
     ],
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -35,3 +35,5 @@ class EstateProperty(models.Model):
         default='new',
     )
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    offer_ids = fields.One2many("estate.property.offer", "property_id")
+    tag_ids = fields.Many2many("estate.property.tag")
