aa39eae05cf028df051b5da049594f40394b3eb8
carlo191 <carloelvisi01@outlook.it>
2026-08-18 18:27:44 +0200

    Fix: Aggiunto modulo 'mail' alle dipendenze nel manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Real Estate',
     'version': '1.0',
     'summary': 'Real estate management module',
-    'depends': ['base'],
+    'depends': ['base', 'mail'],
     'author': 'Carlo',
     'license': 'OPL-1',
     'data': [
