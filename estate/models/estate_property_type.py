e7ec73080b9b3f68a21b29ab9e0e632f9f4d5211
carlo191 <carloelvisi01@outlook.it>
2026-08-18 14:17:11 +0200

    metodo action_open_offer_ids per le offerte del tipo immobile

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -11,3 +11,20 @@ class EstatePropertyType(models.Model):
 
     name = fields.Char(required=True)
     property_ids = fields.One2many("estate.property", "property_type_id")
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id")
+    offer_count = fields.Integer(compute="_compute_offer_count")
+
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
+
+    def action_open_offer_ids(self):
+        return {
+            "name": "Offers",
+            "type": "ir.actions.act_window",
+            "view_mode": "list,form",
+            "res_model": "estate.property.offer",
+            "target": "current",
+            "domain": [("property_type_id", "=", self.id)],
+            "context": {"default_property_type_id": self.id},
+        }
